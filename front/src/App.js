c4dc0949bc3d701aa92552b6c904dce4a7361e67
Ikumi Ito <ikumi.ito.p8@dc.tohoku.ac.jp>
2021-10-26 23:16:08 +0900

    日数を選択してその分だけカレンダーを表示

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useRef, useEffect } from "react";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 function App() {
+  const [needCalendar, setNeedCalendar] = useState(false);
+  const inViewNum = useRef(null);
+  const [views, setViews] = useState({
+    agendaView: {
+      type: "timeGrid",
+      buttonText: "3 day",
+    },
+  });
+  const handleClick = () => {
+    setNeedCalendar(true);
+    setViews((prev) => ({
+      ...prev,
+      agendaView: { ...prev.agendaView, dayCount: inViewNum.current.value },
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <label>
+          予定を生成する日数を入力してください:
+          <input type="number" id="viewNum" ref={inViewNum} />
+        </label>
+        <button type="button" onClick={handleClick}>
+          決定
+        </button>
+      </form>
+      {needCalendar && (
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+          views={views}
+          locale='ja'
+          selectable
+          initialView="agendaView"
+        />
+      )}
+    </>
   );
 }
 
