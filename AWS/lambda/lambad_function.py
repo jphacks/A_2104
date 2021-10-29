8fc0fe31adc4b598cc48df1957e63e4113a417ac
Hiroto <hiroto.kurita.q4@dc.tohoku.ac.jp>
2021-10-29 23:22:10 +0900

    Updated lambda

--- AWS/lambda/lambad_function.py
+++ AWS/lambda/lambad_function.py
@@ -1,5 +1,6 @@
 import os
 import json
+import datetime
 
 import httplib2
 from oauth2client.service_account import ServiceAccountCredentials
@@ -46,21 +47,65 @@ class GoogleCalendar(object):
         except Exception as e:
             raise Exception(f"Error:{e}")
 
+def reset_time(dt)->str:
+    return dt.strftime("%Y-%m-%dT00:00:00+09:00")
+
+def calc_dur(days):
+    start = datetime.datetime.now()
+    end = start + datetime.timedelta(days = days)
+    
+    return reset_time(start),reset_time(end)
+
+def cal_parser(cals):
+    ret = []
+    
+    for i in range(len(cals)):
+        in_dict = {}
+        
+        in_dict["summary"]=cals[i]["summary"]
+        in_dict["location"]=cals[i].get("location")
+        in_dict["start"]=cals[i]["start"]["dateTime"]
+        in_dict["end"]=cals[i]["end"]["dateTime"]
+        
+        ret.append(in_dict)
+    
+    return ret
+
+def cal_validate(cals):
+    cals_ = cals.copy()
+    for i in range(len(cals)):
+        location = cals_[i]["location"]
+        if (location == None) or (location == "自宅") or (location == "オンライン"):
+            cals_.pop(i)
+    
+    return cals_
 
 def lambda_handler(event, context):
     
     email = event["email"]
-    time_start = event["start"]
-    time_end = event["end"]
+    days = int(event["days"])
+    
+    time_start,time_end = calc_dur(days)
+    
+    print(time_start,time_end)
+    
     
     cal = GoogleCalendar()
-    res = (
+    
+    #cals : list [dict]. Length is a number of event within the duaration
+    
+    cals = (
         cal.get_schedule(
             email,
             time_start,
             time_end,
         )
     )
+    
+    parsed_cals = cal_parser(cals) #delete unnecessary infomation
+    validated_cals = cal_validate(parsed_cals) #cal info to calc transportations
+    
+    res = [parsed_cals,validated_cals]
     
 
     return {"statusCode": 200, "body": json.dumps(res)}
